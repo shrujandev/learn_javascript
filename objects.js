43ff114e0696d9369a040b6a6df3728a7ea3e3e4
shrujandev <shrujandev1824@gmail.com>
2021-09-07 08:51:19 +0530

    minor changes

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
 //Method 1
-// var Item = new Object()
-// Item.name = "Hide and Seek"
-// Item.price= 10
-// Item.qty = 100
+var Item = new Object()
+Item.name = "Hide and Seek"
+Item.price= 10
+Item.qty = 100
 
-// document.write("<h1>",Item["name"]," ",Item.name," ",Item.price)
+document.write("<h1>",Item["name"]," ",Item.name," ",Item.price)
 
 //Method 2
 function products(nam,pe,qt)
@@ -14,6 +14,6 @@ function products(nam,pe,qt)
     this.qty = qt
 }
 
-var Item=new products("Monaco",10,200)
+var Item2=new products("Monaco",10,200)
 
-document.write("<h1>",Item["name"]," ",Item.name," ",Item.price)
+document.write("<h1>",Item2["name"]," ",Item2.name," ",Item2.price)
